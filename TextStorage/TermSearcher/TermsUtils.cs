ed9f1a32cca8b18047dca5c515a55b729dc2b395
A.Zhyvchyn <zhyvchyn932@gmail.com>
2016-06-06 21:03:19 +0300

    старт ApplyTerms

--- TextStorage/TermSearcher/TermsUtils.cs
+++ TextStorage/TermSearcher/TermsUtils.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using TextStorage.Models;
 
 namespace TextStorage.TermSearcher
@@ -8,17 +10,35 @@ namespace TextStorage.TermSearcher
     {
         public static void ApplyTerms(TextStorageContext dbContext)
         {
-            var terms = dbContext.Terms.Select(t => new
-            {
-                Id = t.Id,
-                StemmedName = t.StemmedName
-            }).ToList();
+            var terms = dbContext.Terms.OrderByDescending(t => t.StemmedName.Length).ToList();
+			if (terms.Count == 0) return;
             var maxWordsInTerm = terms.Max(t => t.StemmedName.Split(new char[] { ' ' },
                 StringSplitOptions.RemoveEmptyEntries).Length);
+			string[] words = new string[maxWordsInTerm];
+			string temp;
+			int position;
+			StringBuilder sb;
             foreach (var text in dbContext.Texts)
             {
-                
+				temp = "";
+				position = 0;
+				sb = new StringBuilder();
+
             }
         }
+		private static bool IsSeparator(char c)
+		{
+			return c == ' ' || c == '\n' || c == '\t' || c == '\r' || c == '('
+				|| c == ')' || c == '[' || c == ']' || c == '—';
+		}
+		private static int isTerm(string s, List<Term> terms)
+		{
+			var stemmedS = Stemmer.Stem(s);
+			var term = terms.Where(t => t.StemmedName == stemmedS);
+			if (terms.Count > 0)
+				return terms.First().Id;
+			else
+				return -1;
+		}
     }
 }
