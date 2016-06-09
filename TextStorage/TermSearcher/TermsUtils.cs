a497f9a9c1015a07f398bb03cf8741457df7fc97
A.Zhyvchyn <zhyvchyn932@gmail.com>
2016-06-09 16:49:19 +0300

    more ApplyTerms

--- TextStorage/TermSearcher/TermsUtils.cs
+++ TextStorage/TermSearcher/TermsUtils.cs
@@ -6,33 +6,31 @@ using TextStorage.Models;
 
 namespace TextStorage.TermSearcher
 {
-    public class TermsUtils
-    {
-        public static void ApplyTerms(TextStorageContext dbContext)
-        {
-            var terms = dbContext.Terms.OrderByDescending(t => t.StemmedName.Length).ToList();
-			if (terms.Count == 0) return;
-            var maxWordsInTerm = terms.Max(t => t.StemmedName.Split(new char[] { ' ' },
-                StringSplitOptions.RemoveEmptyEntries).Length);
+	public class TermsUtils
+	{
+		public static void ApplyTerms(TextStorageContext dbContext) {
+			var terms = dbContext.Terms.OrderByDescending(t => t.StemmedName.Length).ToList();
+			if (terms.Count == 0)
+				return;
+			var maxWordsInTerm = terms.Max(t => t.StemmedName.Split(new char[] { ' ' },
+				StringSplitOptions.RemoveEmptyEntries).Length);
 			string[] words = new string[maxWordsInTerm];
 			string temp;
 			int position;
 			StringBuilder sb;
-            foreach (var text in dbContext.Texts)
-            {
+			foreach (var text in dbContext.Texts) {
 				temp = "";
 				position = 0;
 				sb = new StringBuilder();
-
-            }
-        }
-		private static bool IsSeparator(char c)
-		{
+				for (position = 0; position < text.TextContentOriginal.Length; position++) {
+				}
+			}
+		}
+		private static bool IsSeparator(char c) {
 			return c == ' ' || c == '\n' || c == '\t' || c == '\r' || c == '('
 				|| c == ')' || c == '[' || c == ']' || c == '—';
 		}
-		private static int isTerm(string s, List<Term> terms)
-		{
+		private static int isTerm(string s, List<Term> terms) {
 			var stemmedS = Stemmer.Stem(s);
 			var term = terms.Where(t => t.StemmedName == stemmedS);
 			if (terms.Count > 0)
@@ -40,5 +38,5 @@ namespace TextStorage.TermSearcher
 			else
 				return -1;
 		}
-    }
+	}
 }
